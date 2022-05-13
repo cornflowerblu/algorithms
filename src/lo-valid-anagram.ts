913ea0485814114daceb94faa7faa2ac50c1e863
Roger Urich <rogerurich@overtime.local>
2022-05-13 00:43:03 -0400

    init

---
+++ src/lo-valid-anagram.ts
@@ -0,0 +1,26 @@
+import { longString1, longString2 } from "./constants/long-strings";
+
+const isAnagram = (str1: string, str2: string): boolean => {
+  const start = performance.now();
+  let counterDict: { [id: string]: number } = {};
+  for (let char of str1) {
+    counterDict[char] = (counterDict[char] ?? 0) + 1;
+  }
+  for (let char of str2) {
+    counterDict[char] = (counterDict[char] ?? 0) - 1;
+  }
+  for (let key in counterDict) {
+    if (counterDict[key] !== 0) {
+      console.log("no match");
+      const end = performance.now();
+      console.log(`Started at: ${start} and ended at: ${end}`);
+      return false;
+    }
+  }
+  console.log("match found");
+  const end = performance.now();
+  console.log(`Started at: ${start} and ended at: ${end}`);
+  return true;
+};
+
+isAnagram(longString1, longString2);
---
+++ src/roger-valid-anagram.ts
@@ -0,0 +1,40 @@
+import { longString1, longString2 } from "./constants/long-strings";
+
+const sort = async (x: Array<string>, y: Array<string>) => {
+  x.sort(), y.sort();
+};
+
+async function rogerValidAnagram(
+  firstString: string,
+  secondString: string
+): Promise<boolean> {
+  const start = performance.now();
+
+  if (firstString.length !== secondString.length) {
+    console.log("Strings are not the same length... aborting.");
+    return false;
+  }
+
+  const arr1 = Array.from(firstString);
+  const arr2 = Array.from(secondString);
+
+  const output = arr2.filter((el) => {
+    return arr1.indexOf(el) !== -1;
+  });
+
+  await sort(output, arr1);
+
+  if (output.toString() === arr1.toString()) {
+    console.log("match found");
+    const end = performance.now();
+    console.log(`Started at: ${start} and ended at: ${end}`);
+    return true;
+  }
+
+  console.log("no match");
+  const end = performance.now();
+  console.log(`Started at: ${start} and ended at: ${end}`);
+  return false;
+}
+
+rogerValidAnagram(longString1, longString2);
